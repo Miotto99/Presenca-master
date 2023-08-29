b187b7e98a4ce7e477a972ca0c3984ecd7169639
Miotto99 <arthurmiottodb@gmail.com>
2023-08-29 17:04:42 -0300

    2908

--- src/main/java/com/MundoSenai/Presenca/controller/C_Pessoas.java
+++ src/main/java/com/MundoSenai/Presenca/controller/C_Pessoas.java
@@ -5,6 +5,7 @@ import com.MundoSenai.Presenca.model.M_Resposta;
 import com.MundoSenai.Presenca.service.S_Pessoa;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
